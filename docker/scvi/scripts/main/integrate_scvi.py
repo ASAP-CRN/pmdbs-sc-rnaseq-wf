e654e99d1d0101bd976cedf24a81718625c2f07a
andy henrie <ergonyc@gmail.com>
2024-11-08 09:05:44 -0800

    prettify scripts with functions and __name__=__main__

--- docker/scvi/scripts/main/integrate_scvi.py
+++ docker/scvi/scripts/main/integrate_scvi.py
@@ -2,100 +2,103 @@ import os
 import argparse
 import scvi
 import anndata as ad
-import scanpy
+
+def integrate_with_scvi(adata: ad.AnnData, batch_key: str, latent_key: str) -> tuple[ad.AnnData, scvi.model.SCVI]:
+    """
+    fit scvi model to AnnData object
+    """
+    
+    
+    ## fixed parameters
+    n_latent = 30
+    n_layers = 1
+    train_size = 0.85
+    scvi_epochs = 200
+    batch_size = 1024
+    # accelerator = 'gpu'
+    # devide = "cuda:0"
+    dispersion = "gene"
+    gene_likelihood = "zinb"
+    latent_distribution = "normal"
+    early_stopping = True
+    ## DEPRICATE these training parameters. defaults are good
+    # plan_kwargs = {"lr_factor": 0.1, "lr_patience": 20, "reduce_lr_on_plateau": True}
+    # early_stopping_patience = 25
+
+  
+    # integrate the data with `scVI`
+    noise = ["doublet_score", "pct_counts_mt", "pct_counts_rb"]
+    categorical_covariate_keys = None
+    scvi.model.SCVI.setup_anndata(
+        adata,
+        layer="counts",
+        batch_key=batch_key,
+        continuous_covariate_keys=noise,
+        categorical_covariate_keys=categorical_covariate_keys,
+    )
+
+    model = scvi.model.SCVI(
+        adata,
+        n_layers=n_layers,
+        n_latent=n_latent,
+        dispersion=dispersion,
+        gene_likelihood=gene_likelihood,
+    )
+
+    model.train(
+        train_size=train_size,
+        max_epochs=scvi_epochs,
+        early_stopping=early_stopping,
+        # early_stopping_patience=early_stopping_patience,
+        # plan_kwargs=plan_kwargs,
+    )
+
+    adata.obsm[latent_key] = model.get_latent_representation()  # type: ignore
+                        
+    return adata, model
 
 
-parser = argparse.ArgumentParser(description="Run scVI integration")
-parser.add_argument(
-    "--latent-key",
-    dest="latent_key",
-    type=str,
-    default="X_scvi",
-    help="Latent key to save the scvi latent to",
-)
-parser.add_argument(
-    "--batch-key",
-    dest="batch_key",
-    type=str,
-    help="Key in AnnData object for batch information",
-)
-parser.add_argument(
-    "--adata-input", dest="adata_input", type=str, help="AnnData object for a dataset"
-)
-parser.add_argument(
-    "--adata-output",
-    dest="adata_output",
-    type=str,
-    help="Output file to save AnnData object to",
-)
-parser.add_argument(
-    "--output-scvi-dir",
-    dest="output_scvi_dir",
-    type=str,
-    help="Output folder to save `scvi` model",
-)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run scVI integration")
+    parser.add_argument(
+        "--latent-key",
+        dest="latent_key",
+        type=str,
+        default="X_scvi",
+        help="Latent key to save the scvi latent to",
+    )
+    parser.add_argument(
+        "--batch-key",
+        dest="batch_key",
+        type=str,
+        help="Key in AnnData object for batch information",
+    )
+    parser.add_argument(
+        "--adata-input", dest="adata_input", type=str, help="AnnData object for a dataset"
+    )
+    parser.add_argument(
+        "--adata-output",
+        dest="adata_output",
+        type=str,
+        help="Output file to save AnnData object to",
+    )
+    parser.add_argument(
+        "--output-scvi-dir",
+        dest="output_scvi_dir",
+        type=str,
+        help="Output folder to save `scvi` model",
+    )
 
-# TODO: optional scvi arguments
+    # TODO: optional scvi arguments
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    # 0. load data
+    adata = ad.read_h5ad(args.adata_input)  # type: ignore
 
+    # 2. process data
+    adata,model = integrate_with_scvi(adata, args.batch_key, args.latent_key)
+    
 
-# Set CPUs to use for parallel computing
-scanpy._settings.ScanpyConfig.n_jobs = -1
-
-## fixed parameters
-n_latent = 30
-n_layers = 1
-train_size = 0.85
-scvi_epochs = 200
-batch_size = 1024
-# accelerator = 'gpu'
-# devide = "cuda:0"
-dispersion = "gene"
-gene_likelihood = "zinb"
-latent_distribution = "normal"
-early_stopping = True
-## DEPRICATE these training parameters. defaults are good
-# plan_kwargs = {"lr_factor": 0.1, "lr_patience": 20, "reduce_lr_on_plateau": True}
-# early_stopping_patience = 25
-
-adata = scanpy.read_h5ad(args.adata_input)  # type: ignore
-
-# just use the counts layer. does this delete the "raw"
-# adata = adata.layers['counts']
-
-# integrate the data with `scVI`
-noise = ["doublet_score", "pct_counts_mt", "pct_counts_rb"]
-
-categorical_covariate_keys = None
-
-scvi.model.SCVI.setup_anndata(
-    adata,
-    layer="counts",
-    batch_key=args.batch_key,
-    continuous_covariate_keys=noise,
-    categorical_covariate_keys=categorical_covariate_keys,
-)
-
-
-model = scvi.model.SCVI(
-    adata,
-    n_layers=n_layers,
-    n_latent=n_latent,
-    dispersion=dispersion,
-    gene_likelihood=gene_likelihood,
-)
-
-model.train(
-    train_size=train_size,
-    max_epochs=scvi_epochs,
-    early_stopping=early_stopping,
-    # early_stopping_patience=early_stopping_patience,
-    # plan_kwargs=plan_kwargs,
-)
-adata.obsm[args.latent_key] = model.get_latent_representation()  # type: ignore
-
-# artifacts
-model.save(args.output_scvi_dir, overwrite=True)
-
-adata.write_h5ad(filename=args.adata_output, compression="gzip")
+    # 3. save the integrated adata and scvi model
+    model.save(args.output_scvi_dir, overwrite=True)
+    adata.write_h5ad(filename=args.adata_output, compression="gzip")
