b4ac2243620ad19d87d18d2f64d6cdd90ae02f08
andy henrie <ergonyc@gmail.com>
2025-05-29 22:45:59 +0100

    update test script

--- test1.sh
+++ test1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ROOT="/home/ergonyc/Projects/ASAP/pmdbs-sc-rnaseq-wf"
+
 # Set variables
 COHORT_ID="pmdbs_sc_rnaseq_cohort_analysis_team-scherzer"
 BATCH_KEY="sample"
@@ -7,16 +9,38 @@ LABEL_KEY="cell_type"
 N_TOP_GENES=5000
 
 # Input/output paths
-
-
 ROOT="/home/ergonyc/Projects/ASAP/pmdbs-sc-rnaseq-wf"
+INPUT_PATH="${ROOT}/data/h5_files"  # Path containing .h5 files
+PREPROCESSED_ADATA_DIR="${ROOT}/data/preprocessed_adata"
 
-# # Merge and plot QC metrics
-# python3 /opt/scripts/main/merge_and_plot_qc.py \
-#     --adata-objects-fofn "${PREPROCESSED_ADATA_DIR}/adata_samples_paths.tsv" \
-#     --adata-output "${COHORT_ID}.merged_adata_object.h5ad" \
-#     --output-metadata-file "${COHORT_ID}.initial_metadata.csv" \
-#     --output-validation-file "${COHORT_ID}.validation_metrics.csv"
+# Create output directory if it doesn't exist
+mkdir -p ${PREPROCESSED_ADATA_DIR}
+
+# Apply prep_metadata.py to all .h5 files in INPUT_PATH
+for h5_file in ${INPUT_PATH}/*.h5; do
+    filename=$(basename -- "$h5_file")
+    sample_id="${filename%.*}"  # Remove extension to get sample_id
+    
+    echo "Processing $h5_file..."
+    
+    /home/ergonyc/mambaforge/envs/scib-nb-cuda12.4/bin/python /opt/scripts/main/prep_metadata.py \
+        --adata-input "$h5_file" \
+        --sample-id "$sample_id" \
+        --batch "batch1" \
+        --dataset "$COHORT_ID" \
+        --team "team-scherzer" \
+        --adata-output "${PREPROCESSED_ADATA_DIR}/${sample_id}.preprocessed.h5ad"
+done
+
+# Create a file listing all preprocessed adata paths for merge_and_plot_qc.py
+find ${PREPROCESSED_ADATA_DIR} -name "*.preprocessed.h5ad" | awk '{print $0"\t"$0}' > "${PREPROCESSED_ADATA_DIR}/adata_samples_paths.tsv"
+
+# Merge and plot QC metrics
+python3 /opt/scripts/main/merge_and_plot_qc.py \
+    --adata-objects-fofn "${PREPROCESSED_ADATA_DIR}/adata_samples_paths.tsv" \
+    --adata-output "${COHORT_ID}.merged_adata_object.h5ad" \
+    --output-metadata-file "${COHORT_ID}.initial_metadata.csv" \
+    --output-validation-file "${COHORT_ID}.validation_metrics.csv"
 
 # Filter and normalize
 /home/ergonyc/mambaforge/envs/scib-nb-cuda12.4/bin/python  /opt/scripts/main/filter.py \
